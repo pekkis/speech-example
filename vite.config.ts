5cea2c2f5cd2a8bdb9612161fb0a449cd60175b5
Mikko Forsström <mikko.forsstrom@yle.fi>
2022-08-11 15:28:49 +0300

    Ribuslk.

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vanillaExtractPlugin(),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
-      devOptions: {
-        enabled: true
-        /* other options */
-      },
-      includeAssets: [
-        "favicon-32x32.png",
-        "favicon-16x16.png",
-        "robots.txt",
-        "apple-touch-icon.png"
-      ],
-      manifest: {
-        name: "Kvaak",
-        short_name: "Kvaak",
-        description: "An ERP for ducks",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ],
-        theme_color: "#ffff00",
-        background_color: "#ffff00",
-        display: "standalone"
-      }
-    })
-  ]
+  plugins: [react(), vanillaExtractPlugin()]
 });
